207381823b0ea6e500dac562546ead927653f59e
sadaf2605 <sadaf2605@gmail.com>
2014-08-23 19:20:41 +0600

    deleted git head marks

--- gis2ools/gis2ools.py
+++ gis2ools/gis2ools.py
@@ -29,15 +29,10 @@ def main():
         return 'help message'
     parser = argparse.ArgumentParser(usage=msg())
     parser.add_argument('command', type=str, help='lots of help')
-<<<<<<< HEAD
-    parser.add_argument('-f', type=str, help='gist file name', default=None)
-    parser.add_argument('-d', type=str, help='description of the gist', default=None)
 
-
-=======
     parser.add_argument('-f', '--file-name', type=str, help='gist file name', default=None)
     parser.add_argument('-d', '--description', type=str, help='description of the gist', default="")
->>>>>>> main/master
+
     parser.add_argument('-u', '--user', help='user help', default=None)
     parser.add_argument('-i', '--id', type=str, help='gist id', default=None)
 
